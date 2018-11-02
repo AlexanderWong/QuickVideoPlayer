dee86c231da30327d98a8f565dd2e40f948d8c92
AlexanderWong <wongalex@umich.edu>
2018-11-02 11:39:49 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import _ from "lodash";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+
+const API_KEY = "AIzaSyB7eCAs5T8x2mNzOq-WWJ_IXtnwL5G5ziw";
+
+// create a new component which will produce some HTML.
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch("surfboards");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+      // this.setState({ videos: videos }) only when key and the property are the same variable name.
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
+
+// Take this component's generated HTML and put it on the page aka in the DOM.
+
+ReactDOM.render(<App />, document.querySelector(".container"));
